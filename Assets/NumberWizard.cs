05bbc89cb258b21a194d4eda27c355a3f82d434e
Zakai Edgington <zedgington00@gmail.com>
2020-02-04 15:50:41 -0500

    assigned the script to camera, now says welcome

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,7 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("welcome");
     }
 
     // Update is called once per frame
